74d09f81b7eb7bbe6ed76c4eec79a68ce905f619
varshaverse <varshaverse247@gmail.com>
2022-12-31 13:26:22 -0800

    coin

---
+++ coin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+srand (time(NULL));
+int coin = rand() % 2;
+
+if(coin==0){    
+std::cout <<"Heads \n";
+
+}
+
+else{
+    std::cout<<"Tails \n";
+}
+
+}
