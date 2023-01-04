841722a2f0dc4025e45c4a3e50917359b2be9406
varshaverse <varshaverse247@gmail.com>
2023-01-03 18:35:36 -0800

    loops

---
+++ loops.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+int a = 1;
+
+do{
+
+if(a==11){
+    a++;
+    continue;
+}
+cout << "Value of a is: " << a << endl;
+a++;
+
+}
+while(a<=11); {
+}
+}
+
+
+
+
