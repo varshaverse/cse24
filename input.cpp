13f7f401dba28b45f6db896362b446cfd9f071a3
varshaverse <107377599+varshaverse@users.noreply.github.com>
2023-04-20 18:56:05 -0700

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string x;
+    string y;
+    getline(cin, x);
+    getline(cin, y);
+
+
+    /* your code goes here */
+    cin >> x;
+    cout<<"My name is "<< x <<"."<<endl;
+    cin >> y;
+    cout<<"I enjoy "<< y << "."<<endl;
+
+
+    return 0;
+}
